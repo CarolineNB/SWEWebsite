152b5a4c73180c0deb935eecdb46742c43dead31
Caroline B <eniloracb4@gmail.com>
2019-08-07 11:13:47 -0600

    commit website

---
+++ About-page/script.js
@@ -0,0 +1,21 @@
+var prevScrollpos = window.pageYOffset;
+
+
+
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (currentScrollPos >= "245"){
+    document.getElementById("navbar").className = "solid";
+  }
+  else{
+    document.getElementById("navbar").className = "transparent";
+  }
+
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
